9257fb04f2e34fe0c8e28cf81e963d9e7ded836d
Davide Cirillo <davide.cirillo@bsc.es>
2022-04-14 18:35:45 +0200

    shrink png

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from dash import Dash, dcc, html, Input, Output
 import os
 import utils
-import ipywidgets as widgets
 
 external_stylesheets = [
     {
@@ -12,36 +11,19 @@ external_stylesheets = [
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.title = "Avocado Analytics: Understand Your Avocados!"
+app.title = "PENGUIN"
 
 server = app.server
 
 dpath = 'graph_tables'
-node_stats = [
-    'degree', 
-    'expression_log2FoldChange', 'expression_padj', 
-    'SNP-binding',
-    'Betweenness_global', 'Degree_global', 'Betweenness_in_cluster',
-    'Betweenness_out_cluster', 'Betweenness_enrichment',
-    'Degree_in_cluster', 'Degree_out_cluster', 'Degree_enrichment',
-    'Betweenness_enrichment_pval', 'Degree_enrichment_pval', 'fisher_test_OR', 'fisher_test_pval'
-]
-edge_stats = [
-    None,
-    'fisher_test_edge_OR', 
-    'fisher_test_edge_pvalue',
-    'SNP-path', 
-    'druggable'
-]
-
 prots=sorted([f[:-10] for f in os.listdir(dpath) if f.endswith('_nodes.tsv')])
+
 app.layout = html.Div([
-    html.H2('Hello World'),
     dcc.Dropdown(prots,
         'FOXA1',
         id='dropdown'
     ),
-    html.Img(id='display-value')
+    html.Img(id='display-value', style={'height':'10%', 'width':'10%'})
 ])
 
 @app.callback(
