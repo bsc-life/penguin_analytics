75cba5bb06d0794edc0052f81b0d3c531d7d2bb7
dcirillo <davide.cirillo@bsc.es>
2022-08-08 19:45:46 +0200

    layout changed

---
+++ settings/config.py
@@ -0,0 +1,23 @@
+
+import os
+
+ENV = "DEV"
+#ENV = "PROD"
+
+
+## server
+host = "0.0.0.0"
+port = int(os.environ.get("PORT", 5000))
+
+
+## info
+app_name = "PENGUIN"
+contacts = ""
+code = "https://github.com/bsc-life/penguin_analytics"
+tutorial = ""
+fontawesome = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+
+about = "PENGUIN (Promoter-ENancher-GUided Interaction Networks) is a tool to prioritize protein-protein interactions in enhancer-promoter loops involved in prostate cancer by integrating several sources of information (high-coverage H3K27ac-HiChIP data, DNA-binding motifs, tissue-specific physical protein interactions, gene expression, and genome-wide genetic variants)."
+
+## fs
+#root = os.path.dirname(os.path.dirname(__file__)) + "/"
